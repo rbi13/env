0f61c2dd24eee988595f4d44f6efc435c499d4cb
bondr <bondr@black.adomain.me>
2019-01-18 08:34:53 -0500

    switches bootstrap back to https

--- install.sh
+++ install.sh
@@ -2,8 +2,8 @@
 
 # clone environment repo
 cd ~/
-# git clone https://github.com/rbi13/env.git
-git clone git@github.com:rbi13/env.git
+git clone https://github.com/rbi13/env.git
+# git clone git@github.com:rbi13/env.git
 
 # apply environment
 sh ~/env/apply.sh
